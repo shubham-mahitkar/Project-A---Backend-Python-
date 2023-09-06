72606569fe25781a8fa023f27498773bfafb9e25
aonamrata <aonamrata@gmail.com>
2023-09-06 21:06:00 +0530

    Fix unit tests

---
+++ tests/unit/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["HOST"] = "dummy"
+os.environ["USER_NAME"] = "dummy"
+os.environ["USER_PASSWORD"] = "dummy"
+os.environ["USER_DB"] = "dummy"
--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,6 +5,5 @@ from users.app import app
 
 @pytest.fixture
 def client():
-    app = app({"TESTING": True})
     with app.test_client() as client:
         yield client
