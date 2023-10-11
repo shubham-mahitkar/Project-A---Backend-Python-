555c24d64f2c8cdb848263a7047d88d174bed512
Shubham Mahitkar <shubham.mahitkar@sts.local>
2023-10-11 18:17:53 +0530

    refactored the code: cypher.py

--- users/connectors/neo4j.py
+++ users/connectors/neo4j.py
@@ -1,9 +1,9 @@
 from neo4j import GraphDatabase
 import os
 
+
 URI = os.getenv('NEO4J_URI')
 AUTH = (os.getenv('NEO4J_USER'), os.getenv('NEO4J_PASSWORD'))
-
 
 drive = None
 with GraphDatabase.driver(URI, auth=AUTH) as driver:
--- users/models/neo4j/cypher.py
+++ users/models/neo4j/cypher.py
@@ -4,52 +4,45 @@ from users.models.sql.query import UserModel
 
 
 class CypherModel:
-
-    def __init__(self):
-        pass
-    
     def get_users_applications_from_neo4j(self):
         records = drive.execute_query(
-        "MATCH (u:USER) -[h:HAS_ACCESS_TO]-> (a:APPLICATION) RETURN u.name, a.name, u.neo_id AS userId",
-        database_="neo4j",
+            "MATCH (u:USER) -[h:HAS_ACCESS_TO]-> (a:APPLICATION) RETURN u.name, a.name, u.neo_id AS userId",
+            database_="neo4j",
         )
-        counter = 0
-        neo4j_lst = []
-        for node in records[0]:
-            my_dict = {}
-            name = node['u.name']
-            application = node['a.name']
-            userId = node['userId']
-            my_dict['name'] = name
-            my_dict['application'] = application
-            my_dict['neo_id'] = userId
-            neo4j_lst.insert(counter, my_dict)
-            counter+=1
+
+        neo4j_lst = [
+            {
+                'name': node['u.name'],
+                'application': node['a.name'],
+                'neo_id': node['userId'],
+            }
+            for node in records[0]
+        ]
+
         return neo4j_lst
-    
+
     def user_details_by_id(self, id):
         records = drive.execute_query(
-        f"MATCH (u:USER) -[h:HAS_ACCESS_TO]-> (a:APPLICATION) WHERE u.neo_id={id} RETURN u, a",
-        database_="neo4j",
+            f"MATCH (u:USER) -[h:HAS_ACCESS_TO]-> (a:APPLICATION) WHERE u.neo_id={id} RETURN u, a",
+            database_="neo4j",
         )
+
         if not records[0]:
             return None
-        else:
-            neo4j_lst = []
-            app_list = []
-            for node in records[0]:
-                my_dict = {}
-                node1 = node['u']
-                user = node1['name']
-                node2 = node['a']
-                application = node2['name']
-                my_dict['name'] = user
-                app_list.append(application)
-                my_dict['application'] = app_list
-                my_dict['neo_id'] = id
-                neo4j_lst.append(my_dict)
-            return neo4j_lst[0]
-    
+
+        neo4j_lst = []
+        app_list = [node['a']['name'] for node in records[0]]
+        user_name = records[0][0]['u']['name']
+
+        neo4j_lst.append({
+            'name': user_name,
+            'application': app_list,
+            'neo_id': id,
+        })
+
+        return neo4j_lst[0]
+
+
     def add_user_application(self, id, user_name, application):
         try:
             for app in application:
@@ -63,11 +56,11 @@ class CypherModel:
 
     def update_user_applications_by_id(self, name, id, application):
         try:
-            drive.execute_query(
+            drive.execute_query(            #delete existing relationship and create new relationship
             "MATCH (u:USER)-[r:HAS_ACCESS_TO]->() WHERE u.neo_id=%s DELETE r WITH u MATCH (a:APPLICATION) WHERE a.name IN %s MERGE (u)-[:HAS_ACCESS_TO]->(a)"%(id, application),
             database_="neo4j",
             )
-            drive.execute_query(
+            drive.execute_query(            #update name property of node if updated
             "MATCH (n:USER {neo_id: %s}) SET n.name = '%s';"%(id, name),
             database_="neo4j",
             )
