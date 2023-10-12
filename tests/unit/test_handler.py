1a5573adbea35209a6e3a37e7257c511bdd6c32e
Shubham Mahitkar <shubham.mahitkar@sts.local>
2023-10-12 12:13:33 +0530

    fixed test cases

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -13,7 +13,7 @@ def test_get_users(mock_cypher_model, mock_user_model, client):
         {'id': 101, 'name': 'shubham', 'email': 'shubham@test.com', 'password': 'foo'},
     ]
     mock_cypher_model().get_users_applications_from_neo4j.return_value = [
-        {'application': 'Facebook', 'neo_id': 100}
+        {'name': 'namrata', 'application': 'Facebook', 'neo_id': 100}
     ]
 
     response = client.get('/users')
@@ -32,75 +32,70 @@ def test_get_users(mock_cypher_model, mock_user_model, client):
             'id': 101,
             'name': 'shubham',
             'password': 'foo',
-            # @todo in this case it should return 'application': []
         }
     ]
     assert actual_result == expected_result
 
 
 # #######Delete#########
-# @patch("users.handlers.UserModel")
-# def test_delete_user(mock_model, client):
-#     """Test DELETE /delete endpoint."""
-#     mock_model().delete_user.return_value = [
-#         "User deleted successfully!"
-#     ]
+@patch("users.handlers.UserModel")
+@patch("users.handlers.CypherModel")
+def test_delete_user(mock_model, mock_cypher_model, client):
+    """Test DELETE /delete endpoint."""
 
-#     response = client.delete('/delete/100')
-#     assert response.status_code == 200
-#     actual_result = json.loads(response.data)
-#     assert actual_result == {"result":'User deleted successfully!'}
+    mock_model().delete_user.return_value = [
+        "User deleted successfully!"
+    ]
+    mock_cypher_model().delete_user_application.return_value = [
+        "User deleted successfully!"
+    ]
+
+    response = client.delete('/delete/100')
+    assert response.status_code == 200
+
+    actual_result = json.loads(response.data)
+    assert actual_result == {"result":'User deleted successfully!'}
 
 
 # ########Create#########
-# @patch("users.handlers.UserModel")
-# def test_add_user(mock_model, client):
-#     """Test POST /add endpoint."""
-#     expected_response = {'id': 234, 'name': 'shubham', 'email': 'shubham@test.com', 'password': 'foo', "application": [
-#             "Facebook"
-#         ]}
-#     mock_model().add_user.return_value = expected_response
-#     response = client.post('/add', json=expected_response)
-#     print("response.json : ", response.json)
-#     assert response.status_code == 200
-#     actual_result = response.json
-#     assert actual_result == expected_response
+@patch("users.handlers.UserModel")
+@patch("users.handlers.CypherModel")
+def test_add_user(mock_neo4j, mock_model, client):
+    """Test POST /add endpoint."""
+    expected_response = {'id': 234, 'name': 'shubham', 'email': 'shubham@test.com', 'password': 'foo', 'application': ['Facebook']}
+    expected_response_neo4j = {'application': ['Facebook']}
+
+    user_model_instance = mock_model.return_value
+    user_model_instance.add_user.return_value = expected_response
+
+    cypher_model_instance = mock_neo4j.return_value
+    cypher_model_instance.add_user_application.return_value = expected_response_neo4j
+
+    response = client.post('/add', json=expected_response)
+    assert response.status_code == 200
+
+    actual_result = response.json
+    assert actual_result == expected_response
 
 
 ########Update#########
-# @patch("users.handlers.UserModel")
-# def test_update_user(mock_model, client):
-#     """Test PUT /update endpoint."""
-#     id = 101
-#     expected_response = {'name': 'shubham', 'email': 'shubham@test.com', 'id':id, 'application': ['Facebook']}
-    
-#     mock_model().update_user.return_value = expected_response
-#     formdatadict = {'name': 'shubham', 'email': 'shubham@test.com', 'id': 101, 'application': ['Facebook']}
-#     response = client.put('/update/101', data=json.dumps(formdatadict), content_type='application/json')
-#     assert response.status_code == 200
-#     actual_result = json.loads(response.data)
-#     assert actual_result == expected_response
+@patch("users.handlers.UserModel")
+@patch("users.handlers.CypherModel")
+def test_update_user(mock_neo4j, mock_model, client):
+    """Test PUT /update endpoint."""
+    id = 101
+    expected_response = {'name': 'shubham', 'email': 'shubham@test.com', 'id': id}
+    expected_response_neo4j = {'application': ['Facebook']}
 
+    user_model_instance = mock_model.return_value
+    user_model_instance.update_user.return_value = expected_response
 
-# @patch("users.handlers.UserModel")
-# def test_get_user(mock_model, client):
-#     """Test GET /user/ endpoint."""
-#     id = 101
-#     expected_response = [
-#     {
-#         "application": [
-#             "Youtube"
-#         ],
-#         "email": "ramupdate2@gmail.com",
-#         "id": id,
-#         "name": "ram",
-#         "password": "pbkdf2:sha256:600000$7blUGE7X47wfLhA5$812e9717924855df00688f81d5c8b84b7a1878af7637d523a286c7a1732c7fff"
-#     }
-# ]
-#     a = mock_model().user.return_value = expected_response
-#     print("a: ", a)
+    cypher_model_instance = mock_neo4j.return_value
+    cypher_model_instance.update_user_applications_by_id.return_value = expected_response_neo4j
 
-#     response = client.get(f"/user/{id}")
-#     assert response.status_code == 200
-#     actual_result = json.loads(response.data)
-#     assert actual_result == expected_response
+    formdatadict = {'name': 'shubham', 'email': 'shubham@test.com', 'id': id, 'application': ['Facebook']}
+    response = client.put(f'/update/{id}', data=json.dumps(formdatadict), content_type='application/json')
+    assert response.status_code == 200
+
+    actual_result = json.loads(response.data)
+    assert actual_result == expected_response
